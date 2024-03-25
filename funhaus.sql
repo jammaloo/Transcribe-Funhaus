daeb387f1bb1ccf8bdddd081e2e99c20de23c3ec
James Freeman <james@jammaloo.com>
2024-03-24 22:39:03 -0400

    Add upload_date to videos

--- funhaus.sql
+++ funhaus.sql
@@ -1,6 +1,7 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE videos(id TEXT PRIMARY KEY NOT NULL, title TEXT NOT NULL, view_count INT NOT NULL, updated_at INTEGER);
+CREATE TABLE videos(id TEXT PRIMARY KEY NOT NULL, title TEXT NOT NULL, view_count INT NOT NULL, updated_at INTEGER, upload_date TEXT NOT NULL);
 CREATE TABLE transcriptions(id TEXT PRIMARY KEY NOT NULL, transcription TEXT NOT NULL, updated_at INTEGER NOT NULL);
 CREATE INDEX view_count on videos(view_count);
+CREATE INDEX upload_date on videos(upload_date);
 COMMIT;
--- importList.ts
+++ importList.ts
@@ -6,18 +6,19 @@ const file = Bun.file(import.meta.dir + '/listing.csv');
 const listing = await file.text();
 
 
-const insert = db.prepare("INSERT OR REPLACE INTO videos (id, title, view_count, updated_at) VALUES ($id, $title, $view_count, $updated_at)");
+const insert = db.prepare("INSERT OR REPLACE INTO videos (id, title, view_count, updated_at, upload_date) VALUES ($id, $title, $view_count, $updated_at, $upload_date)");
 
 const insertVideos = db.transaction(cats => {
     const videos = listing.split("\n");
     for (const video of videos) {
         if(!video) continue;
-        const [$id, $title, $view_count] = video.split(";");
+        const [$id, $title, $view_count, $upload_date] = video.split(";");
         insert.run({
             $id,
             $title,
             $view_count,
             $updated_at: Date.now(),
+            $upload_date,
         });
     }
 
